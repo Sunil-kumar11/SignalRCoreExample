fda6cdb9093a1366098052a3576af99b025eb26e
sunil kumar <sunilkumar.b@bizgaze.in>
2023-11-29 12:51:01 +0530

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCoreExample
+{
+    public class ChatHub :  Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("newMessage", "anonymous", message);
+        }
+        //public async Task SendMessage(string user , string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+    }
+}
